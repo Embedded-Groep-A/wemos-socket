252d0c4d977c10fed7661c3bdffd47d57d4074b7
Thijskamphuis <thijskamphuis@icloud.com>
2025-03-20 14:22:46 +0100

    socket

--- src/server.c
+++ src/server.c
@@ -7,15 +7,16 @@ int main() {
     hostSocket(PORT);
     while (1)
     {
-        char input = scanf("%c", &input);
-        sendData(&input);
-        printf("Data sent: %c\n", input);
-
         char *data = listenForData();
         if (data != NULL) {
             printf("Data received: %s\n", data);
         }
+
+        // Allow user to type data
+        char input[1024];
+        printf("Enter data to send: ");
+        if (fgets(input, sizeof(input), stdin) != NULL) {
+            sendData(input);
+            printf("Data sent: %s\n", input);
+        }
     }
-    
-    return 0;
-}
